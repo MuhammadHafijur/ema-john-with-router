ad8b08884696f136c0f0e4e13860ee1c49c53e17
MuhammadHafijur <hafijur.salah@gmail.com>
2021-10-03 06:36:02 +0600

    added more components

--- src/App.js
+++ src/App.js
@@ -1,12 +1,38 @@
-import './App.css';
-import Header from './components/Header/Header';
-import Shop from './components/Shop/Shop';
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import "./App.css";
+import Header from "./components/Header/Header";
+import Inventory from "./components/Inventory/Inventory";
+import OrderReview from "./components/OrderReview/OrderReview";
+import Shop from "./components/Shop/Shop";
+import NotFound from "./components/NotFound/NotFound";
+import PlaceOrder from "./components/PlaceOrder/PlaceOrder";
 
 function App() {
   return (
     <div>
-      <Header></Header>
-      <Shop></Shop>
+      <Router>
+        <Header></Header>
+        <Switch>
+          <Route exact path="/">
+            <Shop></Shop>
+          </Route>
+          <Route path="/shop">
+            <Shop></Shop>
+          </Route>
+          <Route path="/review">
+            <OrderReview></OrderReview>
+          </Route>
+          <Route path="/inventory">
+            <Inventory></Inventory>
+          </Route>
+          <Route path="/placeorder">
+            <PlaceOrder></PlaceOrder>
+          </Route>
+          <Route path="*">
+            <NotFound></NotFound>
+          </Route>
+        </Switch>
+      </Router>
     </div>
   );
 }
